86a2fe9c09bc31e5a9e3fbd86c7338d85c71c661
Hideo Hattori <hattori-h@klab.com>
2013-04-11 22:25:33 +0900

    update example script

--- examples/simple.py
+++ examples/simple.py
@@ -31,3 +31,14 @@ print(rapidjson.loads("2.14"))
 
 print("=" * 30)
 print(rapidjson.dumps([1, None]))
+
+print("=" * 30)
+json = {
+    "name" : "Jack",
+    "bar" : False,
+    "foo" : True,
+    "age" : 20,
+    "length" : 65.97,
+    "pc" : {"windows": "XP", "linux": None}
+}
+print(rapidjson.dumps(json))
