b645e3cff9e899e0e2789241b90e143e46a9d034
hhatto <hhatto.jp@gmail.com>
2016-01-15 00:19:53 +0900

    ver0.3.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup, Extension
 
 setup(
     name='pyrapidjson',
-    version='0.3',
+    version='0.3.1',
     description='Python Interface for rapidjson(JSON parser and generator).',
     long_description=open('README.rst').read(),
     license='Expat License',
