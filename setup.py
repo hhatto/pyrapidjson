0175e295f8d6b1f8d7e0ebae718adae25e3d5f9f
Hideo Hattori <hattori-h@klab.com>
2017-01-13 17:26:20 +0900

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup, Extension
 
 setup(
     name='pyrapidjson',
-    version='0.4.2',
+    version='0.4.3',
     description='Python Interface for rapidjson(JSON parser and generator).',
     long_description=open('README.rst').read(),
     license='Expat License',
